27818995812b1c118a4467e22e87537069f50a41
Tyler Olson <tolson@dlinc.io>
2023-09-11 12:59:22 -0400

    Initial commit

---
+++ js/context.js
@@ -0,0 +1,12 @@
+/*!
+ * Copyright (c) 2023 Digital Bazaar, Inc. All rights reserved.
+ */
+'use strict';
+
+module.exports = {
+  '@context': {
+    '@protected': true,
+    id: '@id',
+    type: '@type'
+  }
+};
